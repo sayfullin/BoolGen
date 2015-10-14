9e3ac5d30fa1c474673a5f971dc30e92db0ee0c5
Ruslan Sayfullin <uraltus@gmail.com>
2015-10-14 22:05:33 +0500

    init

--- teacher/script.js
+++ teacher/script.js
@@ -39,28 +39,30 @@ app.filter("nl2br", function($filter) {
 app.config(['$translateProvider', function ($translateProvider) {
   $translateProvider.translations('en', {
     'TITLE': 'Boolean Expression Generator',
-    'DESCRIPTION': 'Generator is designed to help computer scince teachers and students with training and checking knowledge',
+    'DESCRIPTION': 'Generator is designed to help computer scince teachers and with training and checking knowledge',
     'OPTIONS': 'Options',
+    'VARIANTS_QUANTITY': 'Variant quantity',
     'EXAMPLE': 'Example',
     'QUANTITY': 'Quantity',
     'REGENERATE': 'Regenegate',
     'TASKS': 'Tasks',
     'ANSWERS': 'Answers',
-    'CHECK': 'Check',
-    'SWITCH_TO_TEACHER': 'Switch to teacher version'
+    'VARIANT': 'Variant',
+    'SWITCH_TO_STUDENT': 'Switch to student version'
   });
 
   $translateProvider.translations('ru', {
     'TITLE': 'Генератор логических выражений',
     'DESCRIPTION': 'Генератор призван помочь ИТ-преподавтелям и студентам с созданием логических выражений для обучения и проверки знаний',
     'OPTIONS': 'Настройки',
+    'VARIANTS_QUANTITY': 'Количество вариантов',
     'EXAMPLE': 'Пример',
     'QUANTITY': 'Количество',
     'REGENERATE': 'Перегенировать',
     'TASKS': 'Задачи',
     'ANSWERS': 'Ответы',
-    'CHECK': 'Проверить',
-    'SWITCH_TO_TEACHER': 'Перейти на версию для преподавателей'
+    'VARIANT': 'Вариант',
+    'SWITCH_TO_STUDENT': 'Перейти на версию для студентов'
   });
 
   $translateProvider.preferredLanguage('ru');
@@ -76,47 +78,58 @@ app.controller('BoolGenCotrloller',['$scope', '$translate', function($scope, $tr
 	}
 
   $scope.expressionTypes = [
-    {type: 'Without varibles', quantity: 1, level: 2, example: "True or False"},
-    {type: 'Without varibles', quantity: 0, level: 3, example: "True or False and False"},
-    {type: 'Without varibles', quantity: 1, level: 4, example: "True or False or not False"},
-    {type: 'With variables', quantity: 0, level: 2, variableCount: 1, example: "A and A; A=true"},
-    {type: 'With variables', quantity: 1, level: 4, variableCount: 2, example: "(A and A) or (B and not B); A=false; B=true"},
-    {type: 'With variables', quantity: 0, level: 5, variableCount: 3, example: "(C and (C and A)) or (C or not C); A=false; B=true; C=false"},
-    {type: 'Arithmetic comparison', quantity: 0, level: 1, example: "-1 - 8 != -9"},
-    {type: 'Arithmetic comparison', quantity: 1, level: 2, example: "(0 - 12 = -8) xor not (1 + 6 > 9)"},
-    {type: 'With any values', quantity: 0, level: 3, variableCount: 2, example: "B and not A"},
-    {type: 'With any values', quantity: 1, level: 5, variableCount: 2, example: "not (B and A xor B) xor not B"}
+    {type: 'Without varibles', quantity: parseInt(quantityArray[0]), level: 2, example: "True or False"},
+    {type: 'Without varibles', quantity: parseInt(quantityArray[1]), level: 3, example: "True or False and False"},
+    {type: 'Without varibles', quantity: parseInt(quantityArray[2]), level: 4, example: "True or False or not False"},
+    {type: 'With variables', quantity: parseInt(quantityArray[3]), level: 2, variableCount: 1, example: "A and A; A=true"},
+    {type: 'With variables', quantity: parseInt(quantityArray[4]), level: 4, variableCount: 2, example: "(A and A) or (B and not B); A=false; B=true"},
+    {type: 'With variables', quantity: parseInt(quantityArray[5]), level: 5, variableCount: 3, example: "(C and (C and A)) or (C or not C); A=false; B=true; C=false"},
+    {type: 'Arithmetic comparison', quantity: parseInt(quantityArray[6]), level: 1, example: "-1 - 8 != -9"},
+    {type: 'Arithmetic comparison', quantity: parseInt(quantityArray[7]), level: 2, example: "(0 - 12 = -8) xor not (1 + 6 > 9)"},
+    {type: 'With any values', quantity: parseInt(quantityArray[8]), level: 3, variableCount: 2, example: "B and not A"},
+    {type: 'With any values', quantity: parseInt(quantityArray[9]), level: 5, variableCount: 2, example: "not (B and A xor B) xor not B"}
   ];
   var langKey = getCookie('langKey');
   if(langKey)
     $translate.use(langKey);
 
   $scope.variantsQuatity = 2;
-  $scope.generatedExpressions = [];
+  $scope.generatedExpression = [];
 
 
   $scope.refresh = function(){
-    $scope.wasChecked = false;
-    $scope.generatedExpressions.length = 0;
+    $scope.emptyGeneratedExpression();
 
-    angular.forEach($scope.expressionTypes, function(expType){
-			var taskTexts = {};
-			for(var j=0; j < expType.quantity; j++){
-        if(expType.type != 'With any values'){
-          do{
-						var newExpression = new BoolGen(expType);
-					}while(taskTexts[newExpression.task] !== undefined);
-					// while for checking not repeating
-					// if it will be same loop make one more step
-					taskTexts[newExpression.task] = true;
-        }else{
-          var newExpression = new BoolGen(expType);
-        }
-        $scope.generatedExpressions.push(newExpression);
-      }
-    })
-    saveQuantityArray();
+    for(var i=0; i < $scope.variantsQuatity; i++){
+      var newVariant = [];
+      angular.forEach($scope.expressionTypes, function(expType){
+				var taskTexts = {};
+				for(var j=0; j < expType.quantity; j++){
+          if(expType.type != 'With any values'){
+            do{
+  						var newExpression = new BoolGen(expType);
+  					}while(taskTexts[newExpression.task] !== undefined);
+  					// while for checking not repeating
+  					// if it will be same loop make one more step
+  					taskTexts[newExpression.task] = true;
+          }else{
+              var newExpression = new BoolGen(expType);
+          }
+					newVariant.push(newExpression);
+				}
+      })
+      $scope.generatedExpression.push(newVariant);
+    }
+		saveQuantityArray();
   };
+
+  $scope.emptyGeneratedExpression = function(){
+    var len = $scope.generatedExpression.length;
+    for(var i=0; i < len; i++){
+      $scope.generatedExpression[i].length = 0;
+    }
+    $scope.generatedExpression.length = 0;
+  }
 
 	var saveQuantityArray = function(){
     var len = $scope.expressionTypes.length;
@@ -126,26 +139,8 @@ app.controller('BoolGenCotrloller',['$scope', '$translate', function($scope, $tr
     setCookie('quantityArray', quantityArray.join('|'), 12);
 	}
 
-  $scope.refresh();
-
-  $scope.check = function(){
-    $scope.wasChecked = true;
-    angular.forEach($scope.generatedExpressions, function(expr){
-      if(expr instanceof Array){
-        for(var i=0, len=expr.length; i<len; i++){
-          expr[i].isCorrect = $scope.checkExpression(expr[i]);
-        }
-      }else{
-        expr.isCorrect = $scope.checkExpression(expr);
-      }
-    })
-  }
-
-  $scope.checkExpression = function(expr){
-    if(expr.userAnwser === undefined)
-      return false;
-    else
-      return !!expr.userAnwser == expr.answer;
+  $scope.isArray = function(data){
+    return data instanceof Array;
   }
 
   $scope.changeLanguage = function (langKey) {
@@ -153,8 +148,5 @@ app.controller('BoolGenCotrloller',['$scope', '$translate', function($scope, $tr
     setCookie('langKey', langKey, 14);
   };
 
-  $scope.isArray = function(data){
-    return data instanceof Array;
-  }
-
+  $scope.refresh();
 }]);
